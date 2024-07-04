b7b9ef6bd042bb6404c9f9539e519fab88a44535
Hamza Momin <hamzaex25@gmail.com>
2024-07-04 14:15:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+import Navigation from './assets/Components/Navigation';
+import HeroSection from './assets/Components/Hero';
+
+const App = () => {
+  return (
+
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
